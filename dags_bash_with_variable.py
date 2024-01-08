899a78885736991d156302385dd51a02203a6d9d
jinho kim <jinho.kim@sobetec.com>
2024-01-08 10:57:41 +0900

    upload

---
+++ dags_bash_with_variable.py
@@ -0,0 +1,9 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    
+) as dag:
---
+++ dags_python_email_xcom.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.email import EmailOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_email_xcom",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2024, 1, 8, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id="something_task")
+    def some_logic(**kwargs):
+        from random import choice
+        return choice(["Success", "Fail"])
+    
+    send_email = EmailOperator(
+        task_id="send_email",
+        to="kjhkjh900@naver.com",
+        subject="{{data_interval_end.in_timezone('Asia/Seoul') | ds}} some_logic 처리결과",
+        html_content="{{data_interval_end.in_timezone('Asia/Seoul') | ds}} 처리결과는 <br> \
+                        {{ti.xcom_pull(task_ids='something_task')}} 했습니다."
+    )
+
+    some_logic() >> send_email
