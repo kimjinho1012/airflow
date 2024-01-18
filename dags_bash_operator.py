3b04a25aa90fc442f593a28408af6f09507e0944
jinho kim <jinho.kim@sobetec.com>
2024-01-18 10:32:22 +0900

    'commit

--- dags_bash_operator.py
+++ dags_bash_operator.py
@@ -1,49 +1,21 @@
-from airflow.sensors.bash import BashSensor
-from airflow.operators.bash import BashOperator
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 import pendulum
 
 with DAG(
     dag_id='dags_bash_sensor',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    schedule='0 6 * * *',
+    start_date=pendulum.datetime(2024,1,18, tz='Asia/Seoul'),
+    schedule='0 0 * * *',
     catchup=False
 ) as dag:
-    
-    sensor_task_by_poke = BashSensor(
-        task_id="sensor_task_by_poke",
-        env={'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
-        bash_command=f''' echo &FILE &&
-                        if [ -f $FILE ]; then
-                            exit 0
-                        else 
-                            exit 1
-                        fi''',
-        poke_interval=30,
-        timeout=60*2,
-        mode='poke',
-        soft_fail=False
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bash_command='echo whoami'
     )
 
-    sensor_task_by_reschedule = BashSensor(
-        task_id="sensor_task_by_reschedule",
-        env={'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'},
-        bash_command=f''' echo &FILE &&
-                        if [ -f $FILE ]; then
-                            exit 0
-                        else 
-                            exit 1
-                        fi''',
-        poke_interval=30,
-        timeout=60*2,
-        mode='reschedule',
-        soft_fail=True
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
+        bash_command='echo $HOSTNAME'
     )
 
-    bash_task = BashOperator(
-        task_id='bash_task',
-        env={'FILE': '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/tvCorona19VaccinestatNew.csv'},
-        bash_command='echo "건수: `cat $FILE | wc -l`"',
-    )
-
-    [sensor_task_by_poke, sensor_task_by_reschedule] >> bash_task
+    bash_t1 >> bash_t2
