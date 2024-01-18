57e460bd10d08ab16323f5068b019affb9e94304
jinho kim <jinho.kim@sobetec.com>
2024-01-18 17:34:32 +0900

    ''

--- dags_bash_with_variable.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-import pendulum
-from airflow.operators.bash import BashOperator
-from airflow.models import Variable
-
-with DAG(
-    dag_id="dags_bash_with_variable",
-    
-) as dag:
