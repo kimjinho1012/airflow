af77115b645fbd745d2452f536836a9ffd48d827
jinho kim <jinho.kim@sobetec.com>
2024-01-18 17:37:46 +0900

    ''

--- dags_bash_with_template.py
+++ dags_bash_with_template.py
@@ -19,8 +19,7 @@ with DAG(
         env={
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE':'{{data_interval_end | ds}}'
-        }
-
+        },
         bash_command='echo $START_DATE && echo $END_DATE'
     )
 
