9f24d8fb33757911eb083ffc5a2fa6d2075e217f
jinho kim <jinho.kim@sobetec.com>
2024-01-19 10:09:18 +0900

    ''

--- dags_python_with_xcom_eg1.py
+++ dags_python_with_xcom_eg1.py
@@ -12,27 +12,23 @@ with DAG(
     
     @task(task_id='python_xcom_push_task1')
     def xcom_push1(**kwargs):
-        ti = kwargs["ti"]
-        ti.xcom_push(key="result1", value="value_1")
-        ti.xcom_push(key="result2", value=[1,2,3])
+        ti = kwargs['ti']
+        ti.xcom_push(key='result1', value='value_1')
+        ti.xcom_push(key='result2', value=[1,2,3])
 
     @task(task_id='python_xcom_push_task2')
     def xcom_push2(**kwargs):
-        ti = kwargs["ti"]
-        ti.xcom_push(key="result1", value="value_2")
-        ti.xcom_push(key="result2", value=[1,2,3,4])
+        ti = kwargs['ti']
+        ti.xcom_push(key='result1', value='value_2')
+        ti.xcom_push(key='result2', value=[1,2,3,4])
+        
 
-    @task(task_id='python_xcom_push_task3')
-    def xcom_push3(**kwargs):
+    @task(task_id='python_xcom_pull_task')
+    def xcom_pull(**kwargs):
         ti = kwargs["ti"]
         value1 = ti.xcom_pull(key="result1")
-        value2 = ti.xcom_push(key="result2", task_ids="python_xcom_push_task1")
+        value2 = ti.xcom_pull(key="result2", task_ids="python_xcom_push_task1")
         print(value1)
         print(value2)
 
-    
-    python_xcom_push_task1 = xcom_push1()
-    python_xcom_push_task2 = xcom_push2()
-    python_xcom_push_task3 = xcom_push3()
-
-    python_xcom_push_task1 >> python_xcom_push_task2 >> python_xcom_push_task3
+    xcom_push1() >> xcom_push2() >> xcom_pull()
