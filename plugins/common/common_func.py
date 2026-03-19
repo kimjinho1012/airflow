1a7e3546fc406e02f5389196de50fb406114d4a3
jinho.kim@sobetec.com <jinho.kim@sobetec.com>
2026-03-19 19:55:14 +0900

    'dags_python_task_decorator'

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,18 @@
+
+    
+def outer_func(target_func):
+    def inner_func():
+        print('target 함수 실행 전 입니다.')
+        target_func()
+        print('target 함수 실행 후 입니다.')
+    
+    return inner_func
+
+@outer_func
 def get_sftp():
     print('sftp 작업을 시작합니다.')
+
+a = outer_func(get_sftp)
+a()
+
+get_sftp()
