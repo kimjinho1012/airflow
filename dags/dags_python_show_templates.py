c60aa8b9c9887fee3b935d9ea36f283c626fc718
jinho.kim@sobetec.com <jinho.kim@sobetec.com>
2026-03-21 16:11:45 +0900

    dags_bash_show_template

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_python_show_template',
+    schedule='30 9 * * *',
+    start_date=pendulum.datetime(2026, 3, 10, tz='Asia/Seoul'),
+    catchup=True
+) as dag:
+    @task(task_id='python_task')
+    def show_template(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+        
+    show_template()
